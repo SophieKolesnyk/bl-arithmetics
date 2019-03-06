4980ad3a94c65a88c539947d8f5e9b687366bf0e
SophieKolesnyk <sophiekolesnik@gmai.com>
2019-03-06 04:19:58 +0200

    Project structure fix. Add gitidnore

--- src/InputManager.java
+++ src/InputManager.java
@@ -9,33 +9,25 @@ import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 class PairErrorCodeString {
     public InputManager.ErrorCode return_code;
     public String return_str;
-
     PairErrorCodeString(){}
-
     PairErrorCodeString(InputManager.ErrorCode i_code, String i_str){
         return_code = i_code;
         return_str = i_str;
     }
 }
-
-
-
 public class InputManager
 {
     public static final String SUCCESS = "Successful input", UNKNOWN_INPUT_FORMAT = "Unknown input format", INVALID_VALUE = "The value does not match the format ", INVALID_INPUT = "IO Exception";
     public enum ErrorCode {
         SUCCESS, UNKNOWN_INPUT_FORMAT, INVALID_VALUE, INVALID_INPUT, REPEAD
     }
-
     public static final int DEC = 1, BL = 2, BIG_DEC = 3;
     public enum format{
         DEC, BL, BIG_DEC
     }
-
     public static ErrorCode getDec(String i_str){
         ErrorCode return_code = ErrorCode.INVALID_VALUE;
         Pattern format_pattern = Pattern.compile("^(-?)([0]{1}|[1-9]{1}[0-9]*)(\\.([0-9]+))?$");
@@ -48,16 +40,13 @@ public class InputManager
         }
         return return_code;
     }
-
     public static ErrorCode getBL(String i_str){ //TO DO 0.14.17.16.15.12.10.6.2.1.0.-1.-5.-7.-8.-9
         ErrorCode return_code = ErrorCode.INVALID_VALUE;
         Pattern format_pattern = Pattern.compile("^(0|1){1}\\.([0]{1}|[1-9]{1}[0-9]*)\\.(\\.?-?[0-9]+)+$");
         Matcher matcher = format_pattern.matcher(i_str);
-
         BLDigit bl_i = new BLDigit();
         if (matcher.find()) {
             int Q =  Integer.parseInt(matcher.group(2));
-
             List<String> partition = new ArrayList(Arrays.asList(matcher.group(0).split("\\.")));
             partition = partition.subList(3, partition.size());
             if(Q==partition.size()){
@@ -67,7 +56,6 @@ public class InputManager
         }
         return return_code;
     }
-
     public static ErrorCode getBigDec(String i_str){
         ErrorCode return_code = ErrorCode.INVALID_VALUE;
         Pattern format_pattern = Pattern.compile("^(-?)([0]{1}|[1-9]{1}[0-9]*)(\\.([0-9]+))?$");
@@ -77,12 +65,10 @@ public class InputManager
         }
         return return_code;
     }
-
     public static ErrorCode selectFormat(){
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = "";
         ErrorCode result;
-
         try {
             System.out.println("Select the input format:");
             System.out.println("1 - decimal;\n2 - bit-logarithmic;\n3 - big becimal;");
@@ -111,7 +97,6 @@ public class InputManager
                     result = ErrorCode.UNKNOWN_INPUT_FORMAT;
                 }
             }
-
             if(result == ErrorCode.SUCCESS) {
                 System.out.println("To continue input 'c'");
                 char c[]  = reader.readLine().toCharArray();
@@ -119,16 +104,13 @@ public class InputManager
                 if(c[0]=='c'){
                     result = ErrorCode.REPEAD;
                 }
-
             }
         }
         catch (IOException e){
             return ErrorCode.INVALID_INPUT;
         }
-
         return result;
     }
-
     public static ErrorCode getInput(){
         ErrorCode result;
         ErrorCode cont;
@@ -140,15 +122,10 @@ public class InputManager
         }
         return result;
     }
-
 }
-
-
-
 /*
     static String add(String rl_number1, String rl_number2) {
         StringBuilder builder = new StringBuilder();
-
         String rate1[] = rl_number1.split("\\.");
         String rate2[] = rl_number2.split("\\.");
         System.out.println(rl_number1 + " + " + rl_number2);
@@ -168,22 +145,14 @@ public class InputManager
             builder.append(rate1[i++]).append('.');
         while (j < rate2.length)
             builder.append(rate2[j++]).append('.');
-
-
         while (hasRepeat(builder.toString()))
             builder = new StringBuilder(replaceRepeat(builder.toString()));
-
         return builder.toString();
     }
-
     static List<Integer> sub(BLDigit rl_number1, BLDigit rl_number2) {
-
         List<Integer> rate1 = new ArrayList(rl_number1.N);
         List<Integer> rate2 = new ArrayList(rl_number2.N);
-
-
         int i, j;
-
         for (; !rate2.isEmpty(); ) {
             i = rate1.size() - 1;
             j = rate2.size() - 1;
@@ -197,22 +166,17 @@ public class InputManager
             if(rate1.isEmpty()) break;
             int rate1_element = rate1.get(i).intValue();
             int rate2_element = rate2.get(j).intValue();
-
             rate1.remove(i);
             rate2.remove(j);
             if (rate1_element == rate2_element)
                 continue;
-
             while (rate2_element < rate1_element)
                 rate1.add(i, rate2_element++);
         }
-
         //for (String element : rate1)
           //  builder.append(element).append('.');
-
         while (hasRepeat(rate1))
             rate1 = new ArrayList(replaceRepeat(rate1));
-
         return rate1;
     }
     */
@@ -222,18 +186,13 @@ public class InputManager
     static int suggesedCompare(BLDigit rl_number1, BLDigit rl_number2, Integer sugN){
         int compare = 0;
         List<Integer> sugg = new ArrayList();
-
-
         sugg.add(sugN);
         if(rl_number2.Q!=0) sugg.addAll(rl_number2.N);
         Collections.sort(sugg, Collections.reverseOrder());
-
         return compare(rl_number1, new BLDigit(0,sugg.size(),sugg));
     }
-
     private static int calculateResNumber(BLDigit rl_number1, BLDigit rl_number2){
         Integer value = 0;
-
         if(compare(rl_number1,rl_number2) >= 0) {
             for(int i = 1; i < precision*2; i++){
                 if(suggesedCompare(rl_number1, rl_number2, i)<=0){
@@ -250,23 +209,17 @@ public class InputManager
         }
         return value;
     }
-
     static BLDigit dev(BLDigit rl_number1, BLDigit rl_number2) {
         System.out.println("rl_number1: "+ rl_number1);
         System.out.println("rl_number2: "+ rl_number2+"\n");
-
         int res_sign = rl_number1.sign*rl_number2.sign;
-
         BLDigit minuend = rl_number1;
-
         List<Integer> result_list = rl_number2.N;
         BLDigit result = rl_number2;
-
         int i = 5;
         while(i>=0){//minuend.Q!=0){
             int value = calculateResNumber(minuend, result);
             System.out.println("value = "+value);
-
             result_list.add(value);
             Collections.sort(result_list, Collections.reverseOrder());
             result =  new BLDigit(res_sign, result_list.size(), result_list);
@@ -279,5 +232,4 @@ public class InputManager
         }
         return result;
     }
-
 */
